67bbe1b62150dba99a1a65052c592b52edd0b60c
Aditya Aggarwal <adityaaggarwal092@gmail.com>
2025-11-24 15:36:59 +0530

    [LIZK-4062] [Build-related-change] Consolidate all the build/test related change/configuration into a single commit for future upgrade integration (#139)
    * ZOOKEEPER-4480: Introduce end to end compatibility tests (#106)
    
    Cherry picked e2e tests from upstream. This checks basic backward compatibility and regression testing.
    
    For original PR : apache@609afd1
    
    Co-authored-by: tison <wander4096@gmail.com>
    
    * Fix the build for branch-3.6 (#115)
    
    This has merged all the changes from li-dev branch.
    There is still a problem where it is not able to find snapshot as it needs
    to look at the local m2 repository.
    
    * [LIZK-4062] [Build-related-change] Consolidate all the build/test related change/configuration into a single commit for future upgrade integration
    
    ---------
    
    Co-authored-by: Rahul Rane <rahulrane50@gmail.com>
    Co-authored-by: tison <wander4096@gmail.com>
    Co-authored-by: Komal Desai <98000016+desaikomal@users.noreply.github.com>
    Co-authored-by: Abhilash Kishore <abhilash1in@gmail.com>

--- zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServer.java
+++ zookeeper-server/src/main/java/org/apache/zookeeper/server/ZooKeeperServer.java
@@ -161,7 +161,7 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
         LOG.info("{} = {}", CLOSE_SESSION_TXN_ENABLED, closeSessionTxnEnabled);
 
         ephemeralNodesTotalByteLimit = Integer.getInteger(EPHEMERAL_NODES_TOTAL_BYTE_LIMIT_KEY, DEFAULT_EPHEMERAL_NODES_TOTAL_BYTE_LIMIT);
-        LOG.info("{} = {}",EPHEMERAL_NODES_TOTAL_BYTE_LIMIT_KEY, ephemeralNodesTotalByteLimit);
+        LOG.info("{} = {}", EPHEMERAL_NODES_TOTAL_BYTE_LIMIT_KEY, ephemeralNodesTotalByteLimit);
     }
 
     // @VisibleForTesting
@@ -185,7 +185,9 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
                 ZooKeeperServer.closeSessionTxnEnabled);
     }
 
-    public static int getEphemeralNodesTotalByteLimit() {return ephemeralNodesTotalByteLimit;}
+    public static int getEphemeralNodesTotalByteLimit() {
+        return ephemeralNodesTotalByteLimit;
+    }
 
     protected ZooKeeperServerBean jmxServerBean;
     protected DataTreeBean jmxDataTreeBean;
